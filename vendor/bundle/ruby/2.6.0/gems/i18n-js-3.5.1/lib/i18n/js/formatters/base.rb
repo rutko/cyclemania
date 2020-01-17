9e475504388eb1988b7a01896bf27c26e944301f
rutko <t.222cm@gmail.com>
2020-01-17 23:38:16 +0900

    ⤴️bundle update

---
+++ vendor/bundle/ruby/2.6.0/gems/i18n-js-3.5.1/lib/i18n/js/formatters/base.rb
@@ -0,0 +1,24 @@
+module I18n
+  module JS
+    module Formatters
+      class Base
+        def initialize(js_extend: false, namespace: nil, pretty_print: false, prefix: nil)
+          @js_extend    = js_extend
+          @namespace    = namespace
+          @pretty_print = pretty_print
+          @prefix = prefix
+        end
+
+        protected
+
+        def format_json(translations)
+          if @pretty_print
+            ::JSON.pretty_generate(translations)
+          else
+            translations.to_json
+          end
+        end
+      end
+    end
+  end
+end
